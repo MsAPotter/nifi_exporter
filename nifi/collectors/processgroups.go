fe79720dd2f04b38e6d7a641cdb70069173a63a9
atasha potter <atasha.potter@ip-172-30-67-67.cv.ops>
2020-08-04 15:11:05 +0000

    wip: removed replace import refs to msiedlarek to msapotter

--- nifi/collectors/processgroups.go
+++ nifi/collectors/processgroups.go
@@ -1,7 +1,8 @@
 package collectors
 
 import (
-	"github.com/msiedlarek/nifi_exporter/nifi/client"
+//	"github.com/msiedlarek/nifi_exporter/nifi/client"
+	"github.com/MsAPotter/nifi_exporter/nifi/client"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
